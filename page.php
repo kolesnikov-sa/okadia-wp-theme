1ed8d249fe935e92297956c46321f8787db3d15e
kolesnikov-sa <kolesnikov-sa@ya.ru>
2021-03-23 10:04:47 +0300

    Updated list styles, added breadcrumbs to page.php and their styles

--- page.php
+++ page.php
@@ -3,7 +3,10 @@ get_header(); ?>
 <div id="main-container">
   <div id="main-content">
     <?php while (have_posts()) {
-      the_post(); ?>
+      the_post();
+      if ( function_exists('yoast_breadcrumb') ) {
+        yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+      } ?>
       <div class="page-title"><?php the_title('<h1>', '</h1>'); ?></div>
       <div class="page-content"><?php the_content(); ?></div>
     <?php } ?>
